0e3e3cfc780e458d0fffd3ca01d23e2229cbcc18
Rushang Patel <roguepanda@Rushangs-MBP.lan>
2023-07-31 16:05:46 -0500

    lab part 5 commit

---
+++ main_app/migrations/0004_finch_toys.py
@@ -0,0 +1,16 @@
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('main_app', '0003_toy_alter_feeding_options_alter_feeding_date'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='finch',
+            name='toys',
+            field=models.ManyToManyField(to='main_app.toy'),
+        ),
+    ]
--- main_app/models.py
+++ main_app/models.py
@@ -8,12 +8,27 @@ MEALS = (
   ('D', 'Dinner'),
 )
 
+
 # Create your models here.
+class Toy(models.Model):
+  name = models.CharField(max_length=50)
+  color = models.CharField(max_length=20)
+
+  def __str__(self):
+    return self.name
+
+  def get_absolute_url(self):
+    return reverse('toys_detail', kwargs={'pk': self.id})
+
+
 class Finch(models.Model):
   name = models.CharField(max_length=100)
   breed = models.CharField(max_length=100)
   description = models.TextField(max_length=250)
   age = models.IntegerField()
+  # Create a M:M relationship with Toy
+  # toys is the Related Manager
+  toys = models.ManyToManyField(Toy)
 
   # Changing this instance method
   # does not impact the database, therefore
@@ -26,6 +41,7 @@ class Finch(models.Model):
 
   def fed_for_today(self):
     return self.feeding_set.filter(date=date.today()).count() >= len(MEALS)
+
 
 class Feeding(models.Model):
   date = models.DateField('Feeding Date')
