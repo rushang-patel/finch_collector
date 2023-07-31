b760664773243abd372ceca40c1ebee23d9695e8
Rushang Patel <roguepanda@Rushangs-MBP.lan>
2023-07-30 22:56:15 -0500

    Lab part 3 complete

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 class Finch(models.Model):
@@ -13,4 +14,5 @@ class Finch(models.Model):
   def __str__(self):
     return f'{self.name} ({self.id})'
 
-
+  def get_absolute_url(self):
+    return reverse('detail', kwargs={'finch_id': self.id})
